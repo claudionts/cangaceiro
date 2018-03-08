f0badab96a29acdd8046aaeff4b6eb5780887b30
claudionts <claudionts@gmail.com>
2018-03-07 22:32:14 -0300

    Manipulando Data através de uma classe externa

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -9,19 +9,19 @@ class NegociacaoController {
 
     adiciona(event) {
         event.preventDefault();
-        let data = new Date(...
-            this._inputData.value
-            .split('-')
-            .map((item, indice) => item - indice % 2)
-        );
-        
+
+        let converter = new DateConverter();
+
+        let data = converter.paraData(this._inputData.value);
+
         let negociacao = new Negociacao(
             data,
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value)
         );
-
         console.log(negociacao);
+        let diaMesAno = converter.paraTexto(negociacao.data);
+        console.log(diaMesAno)
     }
 
 }
