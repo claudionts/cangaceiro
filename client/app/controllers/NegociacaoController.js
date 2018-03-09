8a764326d5789e8d238c7241f60691f0ca62983c
claudionts <claudionts@gmail.com>
2018-03-08 22:16:35 -0300

    Peculiarizando o NegociacaoController.js

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -9,16 +9,25 @@ class NegociacaoController {
     }
 
     adiciona(event) {
-        event.preventDefault();
+        event.preventDefault();      
+        this._negociacoes.adiciona(this._criaNegociacao());
+        console.log(this._negociacoes.paraArray());
+        this._limpaFormulario();
+    }
 
-        let negociacao = new Negociacao(
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
+    }
+
+    _criaNegociacao(){
+        return new Negociacao(
             DateConverter.paraData(this._inputData.value),
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value)
         );
-        this._negociacoes.adiciona(negociacao);
-    
-        console.log(this._negociacoes.paraArray());
     }
 
 }
