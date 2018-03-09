69998addf8609359b0d6ae0fe19d3d9725e05a89
claudionts <claudionts@gmail.com>
2018-03-08 22:33:42 -0300

    Blindando o NegociacaoController.js

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -11,7 +11,6 @@ class NegociacaoController {
     adiciona(event) {
         event.preventDefault();      
         this._negociacoes.adiciona(this._criaNegociacao());
-        console.log(this._negociacoes.paraArray());
         this._limpaFormulario();
     }
 
