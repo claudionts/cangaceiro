b510c00e6168bae6bc093eebc902ca5e2f133866
Claudio Neto <claudionts@gmail.com>
2018-03-10 20:54:07 -0300

    Implementando regra de exibição de mensagem

--- client/app/ui/views/MensagemView.js
+++ client/app/ui/views/MensagemView.js
@@ -4,7 +4,7 @@ class MensagemView {
     }
 
     template(model) {
-        return `<p class="alert alert-info">${model.texto}</p>`;
+        return model.texto ? `<p class="alert alert-info">${model.texto}</p>` : `<p></p>`;
     }
 
     update(model) {
