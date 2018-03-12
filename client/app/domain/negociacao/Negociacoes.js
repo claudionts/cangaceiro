07754b44b8f6f5458291136fc153d346ebb97f89
Claudio Neto <claudionts@gmail.com>
2018-03-12 12:07:10 -0300

    Uso de callback para simlificar o refresh da tabela

--- client/app/domain/negociacao/Negociacoes.js
+++ client/app/domain/negociacao/Negociacoes.js
@@ -1,11 +1,13 @@
 class Negociacoes {
-    constructor() {
+    constructor(armadilha) {
         this._negociacoes = [];
+        this._armadilha = armadilha;
         Object.freeze(this);
     }
 
     adiciona(negociacao) {
         this._negociacoes.push(negociacao);
+        this._armadilha(this);
     }
 
     paraArray() {
@@ -20,6 +22,7 @@ class Negociacoes {
 
     esvazia() {
         this._negociacoes.length = 0;
+        this._armadilha(this);
     }
 
 }
